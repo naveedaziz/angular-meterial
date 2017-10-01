bc118689c87a3a9e345c3088745b98d73d1190ab
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-10-02 01:36:10 +0500

    DED

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var express = require('express');
+
+var path = require('path');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+   res.send('index.html');
 });
 
-app.all('/*', function(req, res, next) {
-    res.sendFile('index.html', { root: __dirname + '/public' });
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+   console.log('Node app is running on port', app.get('port'));
 });
 

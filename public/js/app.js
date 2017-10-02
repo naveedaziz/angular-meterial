c6387b863b1e63f86617a9877e509129f5c32727
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-10-03 01:40:34 +0500

    deD

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,67 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function ($routeProvider, $locationProvider) {
-   $routeProvider
-      .when("/", {
-         templateUrl: "partials/home.html"
+var app = angular.module("myApp", ['ui.router']);
+// app.config(function ($routeProvider, $locationProvider) {
+//    $routeProvider
+//       .when("/", {
+//          templateUrl: "partials/home.html"
+//       })
+//       .when("/organization_commitee", {
+//          templateUrl: "partials/organization_commitee.html"
+//       })
+//       .when("/schedule", {
+//          templateUrl: "partials/schedule.html"
+//       })
+//       .when("/organization_comitee", {
+//          templateUrl: "partials/organization_comitee.html"
+//       })
+//       .when("/message", {
+//          templateUrl: "partials/message.html"
+//       })
+//    //$locationProvider.html5Mode(true).hashPrefix('*');
+// });
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+   $urlRouterProvider.otherwise('/');
+
+   // UI Router States
+   // Inserting Page title as State Param
+   $stateProvider
+      .state('home', {
+         url: '/',
+         templateUrl: 'partials/home.html',
+         params: {
+            title: "Welcome to"
+         }
+
       })
-      .when("/organization_commitee", {
-         templateUrl: "partials/organization_commitee.html"
+      .state('organization', {
+         url: '/organization',
+         templateUrl: 'partials/organization_comitee.html',
+         params: {
+            title: "Organization Commitee"
+         }
       })
-      .when("/schedule", {
-         templateUrl: "partials/schedule.html"
+      .state('message', {
+         url: '/message',
+         templateUrl: 'partials/message.html',
+         params: {
+            title: "Agenda"
+         }
       })
-      .when("/organization_comitee", {
-         templateUrl: "partials/organization_comitee.html"
+      .state('schedule', {
+         url: '/schedule',
+         templateUrl: 'partials/schedule.html',
+         params: {
+            title: "Sponsor"
+         }
       })
-      .when("/message", {
-         templateUrl: "partials/message.html"
+      .state('network', {
+         url: '/network',
+         templateUrl: 'partials/network.html',
+         params: {
+            title: "Accomodation"
+         }
       })
-   //$locationProvider.html5Mode(true).hashPrefix('*');
+   //$locationProvider.html5Mode({ enabled: true, requireBase: false });
+
 });

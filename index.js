03f76660373826e2187285ac2388e9d224d1ea11
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-10-02 18:52:43 +0500

    DE

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
+app.get('/schedule', function (req, res) {
+   res.sendfile(path.join(__dirname, 'public/index.html'));
+});
 
 app.get('/', function (req, res) {
-   res.send('index.html');
+   res.sendfile(path.join(__dirname, 'public/index.html'));
 });
 app.get('/organization_commitee', function (req, res) {
-   res.send('index.html');
+   res.sendfile(path.join(__dirname, 'public/index.html'));
 });
+
 
 
 app.listen(app.get('port'), function () {
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
    $routeProvider
       .when("/", {
          templateUrl: "partials/home.html"
@@ -7,4 +7,8 @@ app.config(function ($routeProvider) {
       .when("/organization_commitee", {
          templateUrl: "partials/organization_commitee.html"
       })
+      .when("/schedule", {
+         templateUrl: "partials/schedule.html"
+      })
+   $locationProvider.html5Mode(true).hashPrefix('*');
 });

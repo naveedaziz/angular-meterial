cd2abc35321a626b80f40646f6bfaa9d064da9f3
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-10-19 19:20:41 +0500

    dED

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,33 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             // UI Router States
             // Inserting Page title as State Param
             $stateProvider
+                  .state('signin', {
+                        url: '/signin',
+                        templateUrl: 'partials/signin.html',
+                        params: {
+                              title: "Login to AFES"
+                        }
+                  })
+                  .state('register', {
+                        url: '/register',
+                        templateUrl: 'partials/register.html',
+                        params: {
+                              title: "Register to AFES"
+                        }
+                  })
+                  .state('home_v2', {
+                        url: '/home_v2',
+                        templateUrl: 'partials/home_v2.html',
+                        params: {
+                              title: "Welcome to AFES"
+                        }
+                  })
                   .state('home', {
                         url: '/',
                         templateUrl: 'partials/home.html',
                         params: {
                               title: "Welcome to AFES"
                         }
-
                   })
                   .state('organization', {
                         url: '/organization',
@@ -72,79 +92,216 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             //$locationProvider.html5Mode({ enabled: true, requireBase: false });
 
       })
-   .controller('AppController', function ($scope, $state, $stateParams, $rootScope, $http) {
+
+   .controller('AppController', function ($scope, $state, $stateParams, $rootScope, $location, $http) {
             $scope.updateTitle = function (state, pre) {
                   $scope.title = pre.params.title;
             }
             $rootScope.$on('$stateChangeSuccess', $scope.updateTitle);
+            $rootScope.currentPath = $location.path();
 
-
+            // Read data from database Start -----
             $scope.get_data = function (table, scope) {
-                  //   if (table != 'users'){
-                  //      var filter = { user_id: localStorage.getItem('user_id') };
-                  //   }else{
-                  var filter = {}
-                  //}
+                  var filter = {};
                   $scope.loadingData = true;
                   var table = clientRef.getTable(table);
                   table.where(filter).read().then(function (data) {
                         $scope[scope] = data;
                         $scope.loadingData = false;
-                        $scope.$apply()
-
+                        $scope.$apply();
                   }, $scope.failure);
             }
+            // Read data from database End -----
+
+
+            // Insert Data in database Start -----
+            $scope.insert_data = function (table, data, redirect) {
+                  if (table != 'users') {
+                        //data.user_id = localStorage.getItem('user_id');
+                  }
+                  // return true;
+                  for (var dat in data) {
+                        if (typeof (data[dat]) == 'object') {
+                              data[dat] = JSON.stringify(data[dat]);
+                        }
+                  }
+                  if ($routeParams.id) {
+                        var table = clientRef.getTable(table);
+                        data.id = $routeParams.id;
+                        for (var ind in data) {
+                              if (data[ind] == null || data[ind] == 'null') {
+                                    delete data[ind];
+                              }
+                        }
+                        table.update(data)
+                              .done(function (insertedItem) {
+                                    var id = insertedItem.id;
+                                    $location.path(redirect)
+                                    $scope.$apply();
+                              }, $scope.failure);
+                  } else {
+                        var table = clientRef.getTable(table);
+                        table.insert(data)
+                              .done(function (insertedItem) {
+                                    var id = insertedItem.id;
+                                    //console.log(id);
+                                    //console.log(redirect)
+                                    $location.path(redirect)
+                                    $scope.$apply()
+                              }, $scope.failure);
+                  }
+
+            }
+            // Insert Data in database End -----
+
+            // Login Start ----
+            $scope.user = {};
+            $scope.signinError = false;
+            $scope.loggedIn = {};
+            if (localStorage.getItem('user_name')) {
+                  $scope.loggedIn = localStorage.getItem('user_name');
+            }
+            // $scope.table = [];
+            if (localStorage.getItem('user_info')) {
+                  $scope.user_info = JSON.parse(localStorage.getItem('user_info'));
+            }
+            $scope.user_info = {};
+            $scope.login = function () {
+                  $scope.loginError = false;
+                  console.log($scope.user);
+                  var table = clientRef.getTable('appuser');
+                  table.where($scope.user).read().then(function (data) {
+                        if (data.length) {
+                              console.log("success")
+                              $scope.user_info = data[0];
+                              localStorage.setItem('user_info', JSON.stringify(data[0]));
+                              localStorage.setItem('user_id', data[0].id);
+                              localStorage.setItem('user_email', data[0].email);
+                              localStorage.setItem('user_name', data[0].name);
+                              $scope.loggedIn = data[0].name;
+                              $location.path('/');
+                              $scope.$apply()
+                        } else {
+                              console.log("errorrrr")
+                              $scope.signinError = true;
+                              $scope.$apply()
+                        }
+                  })
+            }
+            // Login End ---
+
+            // Register start 
+            $scope.register = {};
+            $scope.registerError = false;
+            $scope.registerUser = function() {
+                  console.log($scope.register.email);
+                  $scope.registerError = false;
+                  var table = clientRef.getTable('appuser');
+                  table.where({email:$scope.register.email}).read().then(function (data) {
+                        if (data.length) {
+                              $scope.registerError = true;
+                              console.log("email found");
+                              $scope.$apply();
+                              $('#duplicateEmailError').delay(5000).fadeOut();
+                        } else {
+                              console.log("no email.. can register");
+                              $scope.registerError = false;
+                              // $scope.insert_data('appuser', $scope.register, '/signin');
+
+                              for (var dat in $scope.register) {
+                                    if (typeof (data[dat]) == 'object') {
+                                          data[dat] = JSON.stringify(data[dat]);
+                                    }
+                              }
+                              var table = clientRef.getTable('appuser');
+                              table.insert(data)
+                                    .done(function (insertedItem) {
+                                          var id = insertedItem.id;
+                                          //console.log(id);
+                                          //console.log(redirect)
+                                          $location.path('/signin')
+                                          $scope.$apply()
+                                    }, $scope.failure);
+
+                              $scope.$apply();
+                        }
+                  })
+
+
+            }
+            // Register End
+
 
             $scope.loadSchedule = function(){
-               var filter = {};
                $scope.loadingData = true;
-               $http({
-                  method: 'GET',
-                  url: 'https://fishry-app-services.azurewebsites.net/api/table_read?table=schedule'
-               }).then(function successCallback(response) {
-                  var data = response.data;
-                  $scope.schedule = {};
-                  for(var index in data){
-                     if (data[index].dated){
-                        var dated_pre = data[index].dated.split('T');
-                        var date = dated_pre[0].replace('"','');
-                        var dated = moment(date + ' ' + data[index].start_hours + ':' + data[index].start_minutes).format('ddd (Do MMM)');
-                        if (!$scope.schedule[dated]) {
-                           $scope.schedule[dated] = {};
-                        }
-                        var timeCap = data[index].start_hours + ':' + data[index].start_minutes + '-' + data[index].end_hours + ':' + data[index].end_minutes;
-                        if (!$scope.schedule[dated][timeCap]){
-                           $scope.schedule[dated][timeCap] = {};
-                        }
-                        if (data[index].hall && data[index].hall != ''){
-                           var halls = JSON.parse(data[index].hall);
-                              for(hall in halls){
-                                 if (!$scope.schedule[dated][timeCap][halls[hall]]){
-                                    $scope.schedule[dated][timeCap][halls[hall]] = [];
-                                 }
-                                 $scope.schedule[dated][timeCap][halls[hall]].push({ name: data[index].name, faculty: JSON.parse(data[index].faculty), room : JSON.parse(data[index].room)});
-                              }
-                              
-                        }else{
-                           if (!$scope.schedule[dated][timeCap]['open']){
-                              $scope.schedule[dated][timeCap]['open'] = [];
+                  var filter = {};
+                  $scope.loadingData = true;
+                  $http({
+                     method: 'GET',
+                     url: 'https://fishry-app-services.azurewebsites.net/api/table_read?table=schedule'
+                  }).then(function successCallback(response) {
+                     var data = response.data;
+                     $scope.schedule = {};
+                     for(var index in data){
+                        if (data[index].dated){
+                           var dated_pre = data[index].dated.split('T');
+                           var date = dated_pre[0].replace('"','');
+                           var dated = moment(date + ' ' + data[index].start_hours + ':' + data[index].start_minutes).format('ddd (Do MMM)');
+                           if (!$scope.schedule[dated]) {
+                              $scope.schedule[dated] = {};
                            }
-                           $scope.schedule[dated][timeCap]['open'].push({ name: data[index].name, faculty: JSON.parse(data[index].faculty), room: JSON.parse(data[index].room) })
+                           var timeCap = data[index].start_hours + ':' + data[index].start_minutes + '-' + data[index].end_hours + ':' + data[index].end_minutes;
+                           if (!$scope.schedule[dated][timeCap]){
+                              $scope.schedule[dated][timeCap] = {};
+                           }
+                           if (data[index].hall && data[index].hall != ''){
+                              var halls = JSON.parse(data[index].hall);
+                                 for(hall in halls){
+                                    if (!$scope.schedule[dated][timeCap][halls[hall]]){
+                                       $scope.schedule[dated][timeCap][halls[hall]] = [];
+                                    }
+                                    $scope.schedule[dated][timeCap][halls[hall]].push({ name: data[index].name, faculty: JSON.parse(data[index].faculty), room : JSON.parse(data[index].room)});
+                                 }
+                                 
+                           }else{
+                              if (!$scope.schedule[dated][timeCap]['open']){
+                                 $scope.schedule[dated][timeCap]['open'] = [];
+                              }
+                              $scope.schedule[dated][timeCap]['open'].push({ name: data[index].name, faculty: JSON.parse(data[index].faculty), room: JSON.parse(data[index].room) })
+                           }
+                           $scope.loadingData = false;
                         }
-                        
                      }
-                  }
-                  console.log($scope.schedule);
-               }, $scope.failure);
-            }
-            $scope.activeTab = function(id){
-               $('.nav-tabs .nav-item').children().removeClass('active');
-               $('#tab-' + id).addClass('active');
-               $('.tab-content').children().removeClass('in');
-               $('.tab-content').children().removeClass('active'); 
-               $('#tab-content-'+id).addClass('in'); 
-               $('#tab-content-'+id).addClass('active');
-            }
-      })
+                     console.log($scope.schedule);
+                  }, $scope.failure);
+               }
+               $scope.activeTab = function(id){
+                  $('.nav-tabs .nav-item').children().removeClass('active');
+                  $('#tab-' + id).addClass('active');
+                  $('.tab-content').children().removeClass('in');
+                  $('.tab-content').children().removeClass('active'); 
+                  $('#tab-content-'+id).addClass('in'); 
+                  $('#tab-content-'+id).addClass('active');
+               }
+            
 
-      
+            
+      })
+      .filter('unique', function () {
+            return function (collection, primaryKey, secondaryKey) { //optional secondary key
+                  var output = [],
+                        keys = [];
+
+                  angular.forEach(collection, function (item) {
+                        var key;
+                        secondaryKey === undefined ? key = item[primaryKey] : key = item[primaryKey][secondaryKey];
+
+                        if (keys.indexOf(key) === -1) {
+                              keys.push(key);
+                              output.push(item);
+                        }
+                  });
+
+                  return output;
+            };
+      });

f63af431c545ddc16957968c08b72ff5f8efaa59
Naveed Aziz <naveed.aziz@bramerz.pk>
2017-11-04 16:26:29 +0500

    dE

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var fs = require('fs');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 app.get('/schedule', function (req, res) {
